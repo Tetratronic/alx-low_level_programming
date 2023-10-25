c0f5259525c2eae4e86a8b05930e922a9daeeb95
aymane <aymanebk@gmail.com>
2023-10-25 23:37:43 +0100

    orimes

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "main.c"
+
+/** is_prime_number - checks if a number is prime or not
+ * @n: number to check
+ * Returns: 1 if true, else 0.
+ */
+
+int is_prime(int n)
+{
+	return( n % is_prime(n - (n - 2))
+}
