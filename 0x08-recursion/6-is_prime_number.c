979db10a8c81c52d715e5e492095fe49d8ae8ffa
aymane <aymanebk@gmail.com>
2023-10-26 00:01:19 +0100

    betty

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,17 +2,20 @@
 
 int prime_helper(int n, int i);
 
+
 /** is_prime_number - checks if a number is prime or not
  * @n: number to check
  * Returns: 1 if true, else 0.
  */
 
+
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return(prime_helper(n, n - 1));
+	return (prime_helper(n, n - 1));
 }
+
 
 /** prime_helper - helper adds a counter
  * @n: number to checl
@@ -20,11 +23,12 @@ int is_prime_number(int n)
  * Return: actual result
  */
 
+
 int prime_helper(int n, int i)
 {
-	if ( i == 1)
+	if (i == 1)
 		return (1);
-	if ( n % i == 0 && i > 0)
+	if (n % i == 0 && i > 0)
 		return (0);
 	return (prime_helper(n, i - 1));
 }
