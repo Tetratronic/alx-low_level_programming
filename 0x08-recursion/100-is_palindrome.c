51f25d195dda154cfedee1a1456e4bce61850cbe
aymane <aymanebk@gmail.com>
2023-10-26 03:30:29 +0100

    committing

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * is_palindrome - checks if a string is palindome
+ * @s: strung to check
+ * Return: 1 or 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+
+	if (*s == s[strlen(s) - 1])
+		return (is_palindrome(s + 1));
+	else
+		return (0);
+}
