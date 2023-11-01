8577f74b01e0ef7ca7cbdbff324350f17973488a
aymane <aymanebk@gmail.com>
2023-11-01 01:21:43 +0100

    fine

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,9 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
+
 	arr = malloc(sizeof(char) * size);
 
 	if (arr == NULL)
@@ -20,7 +23,6 @@ char *create_array(unsigned int size, char c)
 
 	for (i = 0; i < size; i++)
 		arr[i] = c;
-	return
-		arr;
 
+	return	(arr);
 }
