d500139ca27b66a0eb4ffd3e4c1fc0fe9eb2d838
aymane <aymanebk@gmail.com>
2023-11-01 01:19:41 +0100

    test

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
-	int i;
+	unsigned int i;
 
 	arr = malloc(sizeof(char) * size);
 
@@ -21,6 +21,6 @@ char *create_array(unsigned int size, char c)
 	for (i = 0; i < size; i++)
 		arr[i] = c;
 	return
-		*arr;
+		arr;
 
 }
