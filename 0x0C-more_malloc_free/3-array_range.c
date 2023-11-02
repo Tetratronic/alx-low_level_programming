7a2d2fd4bf6686427694c66072e3623b7d9a3252
aymane <aymanebk@gmail.com>
2023-11-03 00:03:28 +0100

    arrays

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - initializes an array and fills it from min to max
+ * @min: min range
+ * @max: max range
+ * Return: Pointer to array or NULL
+ */
+
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i;
+	int j = min;
+	if (min > max)
+		return (NULL);
+
+	arr = malloc((max - min + 1) * sizeof(int));
+
+	if (!arr)
+		return (NULL);
+
+	for (i = 0; i <= max; i++)
+		arr[i] = j++;
+
+	return (arr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,4 +4,5 @@
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *, char *, unsigned int);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 #endif /* MAIN_H */
