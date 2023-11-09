2ccd83ea2efe8ec045bca3efba89add34c5aeeff
aymane <aymanebk@gmail.com>
2023-11-09 21:09:21 +0100

    fname

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints name
+ * @name: name to print
+ * @f: pointer to func
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_POINTERS_H
+#define
+
+void print_name(char*, void *);
+
+#endif
